1f52c96f5f49808f539feb3c05f26bd786e1fa5c
rushikesh05 <rushikeshsurywanshi007@gmail.com>
2024-08-21 19:48:57 +0530

    Updates

---
+++ precache-manifest.8ac86c00996df2e2cb50af71f45ab6ca.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c2c2c505c2839a744e39af988cc6c95",
+    "url": "/rushikeshsurywanshi/WeatherApp/index.html"
+  },
+  {
+    "revision": "7cc6184c1b3de335a45a",
+    "url": "/rushikeshsurywanshi/WeatherApp/static/css/main.a7661291.chunk.css"
+  },
+  {
+    "revision": "8e0ef78b07aa71303b3a",
+    "url": "/rushikeshsurywanshi/WeatherApp/static/js/2.09b79422.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/rushikeshsurywanshi/WeatherApp/static/js/2.09b79422.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7cc6184c1b3de335a45a",
+    "url": "/rushikeshsurywanshi/WeatherApp/static/js/main.f089e530.chunk.js"
+  },
+  {
+    "revision": "3562fbd951d8b41879f8",
+    "url": "/rushikeshsurywanshi/WeatherApp/static/js/runtime-main.5ed8ea39.js"
+  },
+  {
+    "revision": "0a218296b39a3569fe9c933e3ababb49",
+    "url": "/rushikeshsurywanshi/WeatherApp/static/media/WeatherIcons.0a218296.gif"
+  },
+  {
+    "revision": "62c7a47b253f3108f95bdbe9883fc35f",
+    "url": "/rushikeshsurywanshi/WeatherApp/static/media/background1.62c7a47b.jpg"
+  },
+  {
+    "revision": "4f1ab76e1f95ce052188c5a75810fe66",
+    "url": "/rushikeshsurywanshi/WeatherApp/static/media/city1.4f1ab76e.jpg"
+  }
+]);
