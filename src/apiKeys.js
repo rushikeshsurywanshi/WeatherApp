2dae8ddc309a29ca26605f41eaf9af20e1e68e6d
rushikesh05 <rushikeshsurywanshi007@gmail.com>
2024-08-21 14:12:57 +0530

    weather

---
+++ src/apiKeys.js
@@ -0,0 +1,5 @@
+// Visit https://api.openweathermap.org & then signup to get our API keys for free
+module.exports = {
+  key: "76544e276093b1a1e8ac51879a4fc663",
+  base: "https://api.openweathermap.org/data/2.5/",
+};
